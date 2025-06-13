80f75b1984581a03110b63ecf630909fa2e14e8c
Ivan Vergés <ivan@pokecode.net>
2025-06-13 12:02:11 +0200

    update decidim

--- lib/decidim/odoo/version.rb
+++ lib/decidim/odoo/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-meetings version.
   module Odoo
     VERSION = "0.2.2"
-    DECIDIM_VERSION = "0.27.9"
+    DECIDIM_VERSION = "0.27.10"
     COMPAT_DECIDIM_VERSION = [">= 0.27.0", "< 0.28"].freeze
   end
 end
